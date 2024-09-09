27ce2f4bed0a78709ca98c1c34145c67ad6780c8
Pama Lee <pama@pamalee.cn>
2024-09-09 20:32:29 +0800

    更改配置

--- site.config.js
+++ site.config.js
@@ -10,12 +10,7 @@ const CONFIG = {
     github: "Pama-Lee",
     instagram: "",
   },
-  projects: [
-    // {
-    //   name: `morethan-log`,
-    //   href: "https://github.com/morethanmin/morethan-log",
-    // },
-  ],
+  projects: [],
   // blog setting (required)
   blog: {
     title: "Jiake Li's Blog",
